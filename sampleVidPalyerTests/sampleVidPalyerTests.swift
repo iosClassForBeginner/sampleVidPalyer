ef7a6bc33969dc535dea2f744e67769c4f0e4f26
WataruMaeda <wm19850716@gmail.com>
2016-03-12 09:21:50 +0900

    Initial Commit

---
+++ sampleVidPalyerTests/sampleVidPalyerTests.swift
@@ -0,0 +1,36 @@
+//
+//  sampleVidPalyerTests.swift
+//  sampleVidPalyerTests
+//
+//  Created by Wataru Maeda on 3/12/16.
+//  Copyright © 2016 wataru maeda. All rights reserved.
+//
+
+import XCTest
+@testable import sampleVidPalyer
+
+class sampleVidPalyerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
